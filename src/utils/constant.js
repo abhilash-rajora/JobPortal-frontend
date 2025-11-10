317d69b45a87cf9f0f06c7f06ee2efc3a9365fcf
Abhilash Rajora <mayankrajora316@gmail.com>
2025-11-10 21:29:24 +0530

    update API URLs

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,9 +1,6 @@
-
-
 const API_URL = import.meta.env.VITE_API_URL;
 
-// src/utils/constant.js
-export const USER_API_END_POINT = "https://jobportal-backend-7xp3.onrender.com/api/v1/user";
-export const JOB_API_END_POINT = "https://jobportal-backend-7xp3.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT = "https://jobportal-backend-7xp3.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT = "https://jobportal-backend-7xp3.onrender.com/api/v1/company";
+export const USER_API_END_POINT = `${API_URL}/user`;
+export const JOB_API_END_POINT = `${API_URL}/job`;
+export const APPLICATION_API_END_POINT = `${API_URL}/application`;
+export const COMPANY_API_END_POINT = `${API_URL}/company`;
